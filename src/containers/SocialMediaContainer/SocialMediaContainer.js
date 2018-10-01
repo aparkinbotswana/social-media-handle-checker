b6b10b21537de1f48a061c7292b89ac48a7e8577
James Theo <jamestheo@Jamess-MacBook-Pro-3.local>
2018-10-01 20:55:16 +1000

    server setup. request to server and response from it being received. getting data that I need from it. Yay

--- src/containers/SocialMediaContainer/SocialMediaContainer.js
+++ src/containers/SocialMediaContainer/SocialMediaContainer.js
@@ -2,31 +2,7 @@ import React, { Component } from 'react'
 import classes from './SocialMediaContainer.css'
 import GithubImage from '../../assets/github.svg'
 import InstagramImage from '../../assets/instagram.svg'
-import TumblrImage from '../../assets/tumblr.svg'
 import TwitterImage from '../../assets/twitter.svg'
-
-
-// const url = {
-//   github: 'https://github.com/',
-//       // exmple https://github.com/aparkinbotswana
-//       // getting Access-Control-Allow-Origin
-//       // bogus request returns 404. successful returns 200
-//   instagram: 'https://www.instagram.com/',
-//       // example https://www.instagram.com/carmen.samdiego/ NOTE!!! the forward slash at the end seems to be part of many user account. typing the web address without the slash will result in a redirect to the same address with the slash at the end and a 301 status code
-//       // not get any Access-Control-Allow-Origin
-//       // bogus request brings back 404. successful request gives 200
-//   tumblr: '.tumblr.com',
-//       // example http://twitter.com/realDonaldTrump
-//       // example https://twitter.com/realDonaldTrump
-//       // figure out best way to handle http and https. hold an additional key with both in an array? I dunno. maybe check for one and the redirect code of the other? 
-//       // Give this some thought. Don't want to include too many special case handles for each social app. Want to try keep it all in on function
-//       // getting Access-Control-Allow-Origin
-//       // bogus page returns 404. successful returns 200. seems there are both http and https pages. also, https redirect gives 302 before redirecting to http, whereas http gives 307 before redirecting to http pages. may need to account for this in code.
-//   twitter: 'https://twitter.com/'
-//       //example https://twitter.com/whatisavariable
-//       // getting Access-Control-Allow-Origin
-//       // bogus page returns 404. successful returns 200. http to https redirects also give a 307 code. This may not be an issue though, it seems all twitter pages are https.
-// }
 
 
 class SocialMediaContainer extends Component {
@@ -35,34 +11,34 @@ class SocialMediaContainer extends Component {
     availability: {
       github: null,
       instagram: null,
-      tumblr: null,
       twitter: null
     }
   }
 
-  // handleResponse = () => {
-  // }
+  handleResponse = (response) => {
+    console.log(response);
+    // this.setState({  })
+
+  }
 
   handleReject = () => {
     // console.log('this has been rejected');
   }
 
-  makeGetRequest = (url) => {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", url);
-      xhr.onload = () => resolve(console.log(xhr.status)
-);        
-      xhr.onerror = () => reject(this.handleReject());
-      xhr.send();
-    });
+  makeGetRequest = (username) => {
+    fetch(`https://aqueous-ocean-13621.herokuapp.com/?u=${username}`).then(function (response) {
+      console.log(response.json());
+      return response.json();
+    })
+      .then(function (myJson) {
+        console.log(myJson);
+      });
   }
 
   componentDidUpdate() {  
     if (this.props.sendRequest) {
       // this.makeGetRequest('https://www.instagram.com/carmen.samdiego/')
-      this.makeGetRequest('https://github.com/aparkinbotswana')
-
+      this.makeGetRequest(this.props.username)
       this.props.handleGetRequest()
       // executes handleGetRequest again so that state for sendRequest can be set back to false
     }
@@ -89,7 +65,6 @@ class SocialMediaContainer extends Component {
       <div className={classes.container}>
         <GithubImage className={imgClass.join(' ')} />
         <InstagramImage className={imgClass.join(' ')} />
-        <TumblrImage className={imgClass.join(' ')} />
         <TwitterImage className={imgClass.join(' ')} />
       </div>
     )
